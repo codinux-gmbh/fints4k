7e729cb503fe522503bcea67293ec98159b7926f
dankl <dankito@users.noreply.github.com>
2019-10-13 20:19:02 +0200

    Re-enabled parameters

--- fints4javaLib/src/main/kotlin/net/dankito/fints/messages/segmente/implementierte/umsaetze/Saldenabfrage.kt
+++ fints4javaLib/src/main/kotlin/net/dankito/fints/messages/segmente/implementierte/umsaetze/Saldenabfrage.kt
@@ -1,7 +1,9 @@
 package net.dankito.fints.messages.segmente.implementierte.umsaetze
 
 import net.dankito.fints.messages.Existenzstatus
+import net.dankito.fints.messages.datenelemente.implementierte.Aufsetzpunkt
 import net.dankito.fints.messages.datenelemente.implementierte.account.AlleKonten
+import net.dankito.fints.messages.datenelemente.implementierte.account.MaximaleAnzahlEintraege
 import net.dankito.fints.messages.datenelementgruppen.implementierte.Segmentkopf
 import net.dankito.fints.messages.datenelementgruppen.implementierte.account.Kontoverbindung
 import net.dankito.fints.messages.segmente.Segment
@@ -15,12 +17,13 @@ open class Saldenabfrage(
     bank: BankData,
     customer: CustomerData, // TODO: pass AccountData instead
     allAccounts: Boolean = false,
-    maxAmountEntries: Int? = null
+    maxAmountEntries: Int? = null,
+    continuationId: String? = null
 )
     : Segment(listOf(
         Segmentkopf(CustomerSegmentId.Balance, 5, segmentNumber),
         Kontoverbindung(bank.countryCode, bank.bankCode, customer.customerId),
-        AlleKonten(allAccounts, Existenzstatus.Mandatory)
-//        MaximaleAnzahlEintraege(maxAmountEntries ?: 0, Existenzstatus.Optional),
-//        Aufsetzpunkt("", Existenzstatus.Optional)
+        AlleKonten(allAccounts, Existenzstatus.Mandatory),
+        MaximaleAnzahlEintraege(maxAmountEntries, Existenzstatus.Optional),
+        Aufsetzpunkt(continuationId, Existenzstatus.Optional)
 ))
