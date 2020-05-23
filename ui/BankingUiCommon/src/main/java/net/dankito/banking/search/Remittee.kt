17b3066044def27b17751e2d967d0865cedf9fa0
dankito <git@dankito.net>
2020-05-23 16:36:19 +0200

    Implemented case insensitive search display of remittee name (so that e.g. for 'John Doe' and 'JOHN DOE' not two different entries are displayed)

--- ui/BankingUiCommon/src/main/java/net/dankito/banking/search/Remittee.kt
+++ ui/BankingUiCommon/src/main/java/net/dankito/banking/search/Remittee.kt
@@ -11,6 +11,25 @@ data class Remittee(
     internal constructor() : this("", "", "") // for object deserializers
 
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Remittee) return false
+
+        if (name.equals(other.name, true)) return false
+        if (iban != other.iban) return false
+        if (bic != other.bic) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = name.toLowerCase().hashCode()
+        result = 31 * result + (iban?.hashCode() ?: 0)
+        result = 31 * result + (bic?.hashCode() ?: 0)
+        return result
+    }
+
+
     override fun toString(): String {
         return name
     }
