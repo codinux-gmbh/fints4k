79a3080bbddb2f56b151e75d71252f91a11c8f8a
dankito <git@dankito.net>
2020-07-12 18:16:08 +0200

    Using now LogToConsoleLogger on iOS due to many bugs of NSLog under Kotlin

--- common/src/iosMain/kotlin/net/dankito/utils/multiplatform/log/DefaultLoggerFactory.kt
+++ common/src/iosMain/kotlin/net/dankito/utils/multiplatform/log/DefaultLoggerFactory.kt
@@ -4,7 +4,9 @@ package net.dankito.utils.multiplatform.log
 actual class DefaultLoggerFactory {
 
     actual fun createDefaultLoggerFactory(): ILoggerFactory {
-        return NSLogLoggerFactory()
+        // i cannot recommend using NSLog from Kotlin, has many bugs (e.g. cutting messages, printing non Ansi characters in Ansi messages, ...)
+//        return NSLogLoggerFactory()
+        return LogToConsoleLoggerFactory()
     }
 
 }
