8f15bd25b39d71682e699c2f54f95a072349b535
dankito <git@dankito.net>
2020-08-04 00:53:34 +0200

    Implemented resizing TAN image

--- ui/BankingiOSApp/BankingiOSApp/ui/views/ImageTanView.swift
+++ ui/BankingiOSApp/BankingiOSApp/ui/views/ImageTanView.swift
@@ -8,17 +8,64 @@ struct ImageTanView: View {
     
     private var imageData: Data
     
+    @State private var imageWidth: CGFloat
+    
+    private let imageMinWidth: CGFloat
+    
+    private let imageMaxWidth: CGFloat
+    
+    private let step: CGFloat
+    
     
     init(_ tanChallenge: ImageTanChallenge) {
         self.tanChallenge = tanChallenge
         
         self.imageData = tanChallenge.image.imageBytesAsNSData()
+        
+        let screenWidth = UIScreen.main.bounds.width
+        let screenWidthQuarter = screenWidth / 4
+        
+        self.imageMinWidth = screenWidthQuarter < 150 ? 150 : screenWidthQuarter // don't know whey but iOS seems that it doesn't scale image smaller than 150
+        self.imageMaxWidth = screenWidth
+        
+        let range = imageMaxWidth - imageMinWidth
+        
+        self._imageWidth = State(initialValue: imageMinWidth + range / 2)
+        
+        self.step = range / 20
     }
     
     
     var body: some View {
-        UIKitImageView(data: self.imageData)
+        Section {
+            HStack {
+                Text("Size")
+                
+                Spacer()
+                
+                Rectangle()
+                    .fill(Color.gray)
+                    .frame(width: 6, height: 9)
+                
+                Slider(value: $imageWidth, in: imageMinWidth...imageMaxWidth, step: step)
+                
+                Rectangle()
+                    .fill(Color.gray)
+                    .frame(width: 16, height: 19)
+            }
+            
+            HStack {
+                Spacer()
+                
+                UIKitImageView(data: self.imageData)
+                    .frame(width: imageWidth, height: imageWidth)
+                    .padding(.horizontal, -32.0)
+                
+                Spacer()
+            }
+        }
     }
+    
 }
 
 
