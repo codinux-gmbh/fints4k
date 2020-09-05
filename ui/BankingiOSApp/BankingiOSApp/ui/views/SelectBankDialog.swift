147ce950f798be87a5b7acdc8fe2d479a791b2b7
dankito <git@dankito.net>
2020-09-05 03:30:29 +0200

    Removed setting selectedBank to nil for unsupported banks, there should be absolutely no use in it

--- ui/BankingiOSApp/BankingiOSApp/ui/views/SelectBankDialog.swift
+++ ui/BankingiOSApp/BankingiOSApp/ui/views/SelectBankDialog.swift
@@ -107,8 +107,6 @@ struct SelectBankDialog: View {
     }
     
     private func showBankIsNotSupportedMessage(_ bank: BankInfo) {
-        self.selectedBank = nil
-        
         self.errorMessage = Message(title: Text("\(bank.name) does not support FinTS 3.0"), message: Text("Only banks supporting FinTS 3.0 can be used in this app."))
     }
     
