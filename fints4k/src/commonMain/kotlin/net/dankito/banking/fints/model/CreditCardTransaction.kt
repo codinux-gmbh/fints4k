b94c5d5cf5433e2e1a091db9f3f42abbc3866d07
dankito <git@dankito.net>
2020-09-24 03:18:02 +0200

    Added toString()

--- fints4k/src/commonMain/kotlin/net/dankito/banking/fints/model/CreditCardTransaction.kt
+++ fints4k/src/commonMain/kotlin/net/dankito/banking/fints/model/CreditCardTransaction.kt
@@ -1,6 +1,7 @@
 package net.dankito.banking.fints.model
 
 import net.dankito.utils.multiplatform.Date
+import net.dankito.utils.multiplatform.format
 
 
 open class CreditCardTransaction(
@@ -9,4 +10,10 @@ open class CreditCardTransaction(
     val bookingDate: Date,
     val valueDate: Date,
     val isCleared: Boolean
-)
+) {
+
+    override fun toString(): String {
+        return "${valueDate.format("dd.MM.yy")} $amount $otherPartyName"
+    }
+
+}
