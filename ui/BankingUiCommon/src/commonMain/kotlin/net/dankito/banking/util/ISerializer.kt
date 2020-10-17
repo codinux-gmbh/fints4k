5c4a4a485f44f389198c7330ad0eba40d7037629
dankito <git@dankito.net>
2020-10-17 23:03:28 +0200

    Forgot to commit serializer methods for AuthenticationSettings

--- ui/BankingUiCommon/src/commonMain/kotlin/net/dankito/banking/util/ISerializer.kt
+++ ui/BankingUiCommon/src/commonMain/kotlin/net/dankito/banking/util/ISerializer.kt
@@ -6,9 +6,17 @@ import kotlin.reflect.KClass
 
 interface ISerializer {
 
+    fun serializeObjectToString(obj: Any): String?
+
     fun serializeObject(obj: Any, outputFile: File)
 
+
+    fun <T : Any> deserializeObject(serializedObject: String, objectClass: KClass<T>, vararg genericParameterTypes: KClass<*>): T?
+
     fun <T : Any> deserializeObject(serializedObjectFile: File, objectClass: KClass<T>, vararg genericParameterTypes: KClass<*>): T?
+
+    fun <T : Any> deserializeListOr(serializedObject: String, genericListParameterType: KClass<T>,
+                                    defaultValue: List<T> = listOf()) : List<T>
 
     fun <T : Any> deserializeListOr(serializedObjectFile: File, genericListParameterType: KClass<T>,
                                     defaultValue: List<T> = listOf()) : List<T>
