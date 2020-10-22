44e15e4aebae6bd75ced5c3856f57506f6c4e5b0
dankito <git@dankito.net>
2020-10-23 00:48:57 +0200

    Implemented locking screen orientation to portrait on phones as there some dialogs / EditTexts are almost unusable. TODO: setting this in BaseActivity is quite bad

--- ui/BankingAndroidApp/src/main/java/net/dankito/banking/ui/android/activities/BaseActivity.kt
+++ ui/BankingAndroidApp/src/main/java/net/dankito/banking/ui/android/activities/BaseActivity.kt
@@ -1,7 +1,9 @@
 package net.dankito.banking.ui.android.activities
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import net.dankito.banking.ui.android.R
 import net.dankito.banking.ui.android.di.BankingComponent
 import net.dankito.banking.ui.android.util.CurrentActivityTracker
 import org.slf4j.LoggerFactory
@@ -24,8 +26,17 @@ abstract class BaseActivity : AppCompatActivity() {
     }
 
 
+    open val isRunningOnTablet: Boolean
+        get() = resources.getBoolean(R.bool.isTablet)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         log.info("Creating Activity $this")
+
+        // a bit bad as it not clearly visible that orientation lock is done here
+        if (isRunningOnTablet == false) { // lock screen to portrait mode on phones as there app really looks bad and some EditTexts are almost unusable
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        }
 
         super.onCreate(savedInstanceState)
     }
