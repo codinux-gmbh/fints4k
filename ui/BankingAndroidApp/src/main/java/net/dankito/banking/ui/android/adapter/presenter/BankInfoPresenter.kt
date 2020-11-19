bfa6b1a2814c8b88c66d7bb46011c7f7968772ac
dankito <git@dankito.net>
2020-11-19 18:33:21 +0100

    Added horizontal lines between BankInfo items

--- ui/BankingAndroidApp/src/main/java/net/dankito/banking/ui/android/adapter/presenter/BankInfoPresenter.kt
+++ ui/BankingAndroidApp/src/main/java/net/dankito/banking/ui/android/adapter/presenter/BankInfoPresenter.kt
@@ -7,6 +7,7 @@ import kotlinx.coroutines.*
 import net.dankito.banking.ui.android.adapter.BankListAdapter
 import net.dankito.banking.ui.presenter.BankingPresenter
 import net.dankito.banking.bankfinder.BankInfo
+import net.dankito.banking.ui.android.extensions.addHorizontalItemDivider
 
 
 open class BankInfoPresenter(protected val presenter: BankingPresenter, context: Context) : RecyclerViewPresenter<BankInfo>(context) {
@@ -17,6 +18,8 @@ open class BankInfoPresenter(protected val presenter: BankingPresenter, context:
 
 
     override fun instantiateAdapter(): RecyclerView.Adapter<*> {
+        recyclerView?.addHorizontalItemDivider()
+
         return adapter
     }
 
