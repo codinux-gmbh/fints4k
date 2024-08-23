88f6d53ccb53fa289575613ea15f087a1acc7476
dankito <git@dankito.net>
2024-08-23 12:47:14 +0200

    Temporarily disabling iOS DateFormatter as it conflicts with nativeMain's DateFormatter. TODO: Fix this

--- fints4k/src/iosMain/kotlin/net/dankito/banking/fints/mapper/DateFormatter.kt
+++ fints4k/src/iosMain/kotlin/net/dankito/banking/fints/mapper/DateFormatter.kt
@@ -1,17 +1,17 @@
-package net.dankito.banking.fints.mapper
-
-import kotlinx.datetime.LocalDate
-import kotlinx.datetime.TimeZone
-
-actual class DateFormatter actual constructor(val pattern: String): NSDateFormatter() {
-
-
-    actual fun parseDate(dateString: String): LocalDate? {
-        this.dateFromString(dateString)?.let { nsDate ->
-            return nsDate.toKotlinInstant().toLocalDateTime(TimeZone.Companion.currentSystemDefault())?.date
-        }
-
-        return null
-    }
-
-}
+//package net.dankito.banking.fints.mapper
+//
+//import kotlinx.datetime.LocalDate
+//import kotlinx.datetime.TimeZone
+//
+//actual class DateFormatter actual constructor(val pattern: String): NSDateFormatter() {
+//
+//
+//    actual fun parseDate(dateString: String): LocalDate? {
+//        this.dateFromString(dateString)?.let { nsDate ->
+//            return nsDate.toKotlinInstant().toLocalDateTime(TimeZone.Companion.currentSystemDefault())?.date
+//        }
+//
+//        return null
+//    }
+//
+//}
